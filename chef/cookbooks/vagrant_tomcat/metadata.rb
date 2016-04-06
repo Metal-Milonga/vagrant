c0427b7a58a52518e645788dc12e696d31b3380b
David Holton <it.arcanum@gmail.com>
2016-04-06 09:20:02 -0400

    adding metadatafile for new tomcat cookbook.

---
+++ chef/cookbooks/vagrant_tomcat/metadata.rb
@@ -0,0 +1,14 @@
+name 'vagrant_tomcat'
+maintainer 'David Holton'
+maintainer_email 'DavidHolton.hpc@gmail.com'
+license 'Apache 2.0'
+description 'Customizes tomcat-latest cookbook from Chef Supermarket for use with Vagrant.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.dm'))
+version '0.0.1'
+
+depends 'java'
+depends 'tomcat_latest'
+
+supports 'centos'
+
+recipe 'vagrant_tomcat::default', 'Updates Tomcat Installation'
